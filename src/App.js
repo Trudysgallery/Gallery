37ec0be7d13323c221d33ee8dbd04b1626ffb7cc
Daniel DeVeau <daniel.deveau@appian.com>
2021-05-24 20:53:45 -0400

    Added HomeCarousel as HomePage and Gallery as separate tab to become shopping tab.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import About from './pages/About.js'; 
 import Contact from './pages/Contact.js'; 
 import Gallery from './pages/Gallery.js'; 
+import HomeCarousel from './components/HomeCarousel/HomeCarousel.js'; 
 import { EasybaseProvider } from 'easybase-react';
 import ebconfig from './ebconfig';
 import Welcome from './components/Welcome/Welcome.js'
@@ -13,15 +14,16 @@ function App() {
     <EasybaseProvider ebconfig={ebconfig}>
       <BrowserRouter>
         <div className="App">
-          <Welcome />
+          <Link to="/"><Welcome /></Link>
           <div className="Navigation">
           <div className="navigation-sub">
-              <Link to="/" className="nav-item">Gallery</Link>
+              <Link to="/gallery" className="nav-item">Gallery</Link>
               <Link to="/about" className="nav-item">About</Link>
               <Link to="/contact" className="nav-item">Contact Me</Link>
             </div>
           </div>
-          <Route exact path="/" component={Gallery} />
+          <Route exact path="/" component={HomeCarousel} />
+          <Route path="/gallery" component={Gallery} />
           <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
         </div>
